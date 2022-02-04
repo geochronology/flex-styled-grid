5c8c38dc64dd84e79f59a3e293ae6567833003a6
geochronology <54387644+geochronology@users.noreply.github.com>
2022-02-04 14:59:09 -0800

    made a thing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+
+const Grid = styled.div`
+  border: 1px solid red;
+`;
+
+const Row = styled.div`
+  display: flex;
+`;
+
+const media = {
+  xs: (styles) => `
+    @media only screen and (max-width: 480px) {
+      ${styles}
+    }
+  `
+}
+
+const Col = styled.div`
+  flex: ${(props) => props.size};
+  /* border: 1px solid green;
+  padding: 16px; */
+  ${(props) => props.collapse && media[props.collapse](`
+    display: none;
+  `)}
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Grid>
+        <Row>
+          <Col size={1}>
+            A nice column
+          </Col>
+        </Row>
+        <Row>
+          <Col size={2}>
+            A twice bigly column
+          </Col>
+          <Col size={1} collapse="xs">
+            Poof!
+          </Col>
+          <Col size={1}>
+            Foofaloopa
+          </Col>
+        </Row>
+      </Grid>
     </div>
   );
 }
